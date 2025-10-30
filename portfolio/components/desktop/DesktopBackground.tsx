8f89339cab8779b4e3538a46cad0a03a60493861
Cqctxs <seanzhao2007@gmail.com>
2025-10-30 01:10:26 -0400

    update the camera animation

--- portfolio/components/desktop/DesktopBackground.tsx
+++ portfolio/components/desktop/DesktopBackground.tsx
@@ -2,7 +2,7 @@
 
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
-import DesktopScene from "./DesktopScene";
+import { Model, CameraAnimation } from "./DesktopScene";
 
 export default function DesktopBackground() {
   return (
@@ -13,10 +13,19 @@ export default function DesktopBackground() {
           background: "linear-gradient(to bottom, #0f0724, #1a0f38, #0d021d)",
         }}
       />
-      <Canvas gl={{ antialias: true, alpha: true }}>
+      <Canvas
+        gl={{ antialias: true, alpha: true }}
+        camera={{
+          position: [0, 2, -8],
+          fov: 75,
+          near: 0.1,
+          far: 1000, // Increase far clipping plane to see distant objects
+        }}
+      >
         <color attach="background" args={["#0d021d"]} />
+        <CameraAnimation />
         <Suspense fallback={null}>
-          <DesktopScene />
+          <Model />
         </Suspense>
       </Canvas>
     </div>
